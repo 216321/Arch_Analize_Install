42f56d1ca4ffdc208d4a44af777632f6a5108cc9
216321 <110435555+216321@users.noreply.github.com>
2025-03-27 23:05:47 -0400

    Added to Install.sh

--- Install.sh
+++ Install.sh
@@ -5,10 +5,26 @@ drive=0
 partition1=0
 partition2=0
 luks=0
+lukspasswd=0
+lukspart=0
 tpm=0
+vbox=0
+sysop=0
+non-sudo=0
 
 # Check for internet.
 ping -c 1 example.com || { echo "Please connect to the internet and wait for the repo test to complete before running."; exit 1; }
+
+# Ask if it is in Virtualbox
+echo -n "Is this machine running in Virtualbox? (y/N) "
+read -n 1 vbox
+if [[ "$vbox" == "Y" ]]; then
+  vbox="1"
+elif [[ "$tpm" == "y" ]]; then
+  vbox="1"
+else
+  vbox="0"
+fi
 
 # Prompt user for the drive.
 lsblk
@@ -22,6 +38,7 @@ if [[ "$luks" == "Y" ]]; then
   luks="1"
   echo -n "LUKS password: "
   read lukspasswd
+  
   # Ask for tpm unlocking.
   echo -n "Do you want tpm unlocking (Requires TPM 2.0)? (y/N) "
   read -n 1 tpm
@@ -36,6 +53,7 @@ elif [[ "$luks" == "y" ]]; then
   luks="1"
   echo -n "LUKS password: "
   read lukspasswd
+  
   # Ask for TPM unlocking (again).
   echo -n "Do you want tpm unlocking (Requires TPM 2.0)? (y/N) "
   read -n 1 tpm
@@ -59,17 +77,41 @@ else
   partition2="${drive}2"
 fi
 
+# Ask for account passwords.
+echo -n "Password for sysop account (Admin): "
+read sysop
+echo -n "Password for non-sudo account (non-admin): "
+read non-sudo
+
 # Start partitioning
-parted $drive --script 'mklabel gpt mkpart "EFI system partition" fat32 1MiB 1025MiB set 1 esp on mkpart "crypt_root" ext4 1025MiB 100% type 2 4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709'
+parted $drive --script 'mklabel gpt mkpart "EFI system partition" fat32 1MiB 1025MiB set 1 esp on mkpart "root" fat32 1025MiB 100% type 2 4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709'
 
-# Crypt setup
+# Crypt setup, formating, drive mounting, swapfile.
 if [[ "$luks" == "1" ]]; then
-
+  echo $lukspasswd | cryptsetup -q luksFormat $partition2
+  echo $lukspasswd | cryptsetup open $partition2 root
+  lukspart="/dev/mapper/root"
+  mkfs.ext4 $lukspart
+  mount $lukspart /mnt
+else
+  mkfs.ext4 $partition2
+  mount $partition2 /mnt
 fi
+mount --mkdir $partition1 /mnt/boot
+mkswap -U clear --size 4G --file /mnt/swapfile
+swapon /mnt/swapfile
 
 # Add sublimetext repo
 curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
 echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 
-####Tack on after chroot
-echo "GSK_RENDERER=gl" >> /etc/environment
+# Resync with repos
+pacman -Sy
+
+# Begin the install.
+if [ "$vbox" == 1 ] && [ "$luks" == 1 ] && [ "$tpm" == 1 ]; then
+
+fi
+
+# Tack on during chroot
+#echo "GSK_RENDERER=gl" >> /etc/environment
