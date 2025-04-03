20fe3ed72576efe6b910a2ea501cc987d5b6bad8
216321 <110435555+216321@users.noreply.github.com>
2025-04-03 23:02:19 +0000

    large additions

--- Install.sh
+++ Install.sh
@@ -13,15 +13,27 @@ lukspart=0
 tpm=0
 vbox=0
 sysop=0
-non-sudo=0
+non_sudo=0
 firewall=0
+hostname=0
+locale_string=0
 
 # Check for internet.
 ping -c 1 example.com || { echo "Please connect to the internet and wait for the repo test to complete before running."; exit 1; }
 
+# Ask for hostname.
+echo -n "Hostname? "
+read hostname
+echo ""
+
+# Ask for locale string.
+echo -n "What is the locale? "
+read locale_string
+
 # Ask if it is in Virtualbox
 echo -n "Is this machine running in Virtualbox? (y/N) "
 read -n 1 vbox
+echo ""
 if [[ "$vbox" == "Y" ]]; then
   vbox="1"
 elif [[ "$tpm" == "y" ]]; then
@@ -31,8 +43,9 @@ else
 fi
 
 # Prompt for host based firewall with default deny rule.
-ehco -n "Do you want nftables installed with a default deny in rule? (y/N) "
-reead -n 1 firewall
+echo -n "Do you want nftables installed with a default deny in rule? (y/N) "
+read -n 1 firewall
+echo ""
 if [[ "$firewall" == "Y" ]]; then
   firewall="1"
 elif [[ "$firewall" == "y" ]]; then
@@ -45,10 +58,12 @@ fi
 lsblk
 echo -n "Enter the drive you wish to install on: "
 read drive
+echo ""
 
 # Ask for encryption.
 echo -n "Do you want an encrypted root? (y/N) "
 read -n 1 luks
+echo ""
 if [[ "$luks" == "Y" ]]; then
   luks="1"
   echo -n "LUKS password: "
@@ -57,6 +72,7 @@ if [[ "$luks" == "Y" ]]; then
   # Ask for tpm unlocking.
   echo -n "Do you want tpm unlocking (Requires TPM 2.0)? (y/N) "
   read -n 1 tpm
+  echo ""
   if [[ "$tpm" == "Y" ]]; then
     tpm="1"
   elif [[ "$tpm" == "y" ]]; then
@@ -68,10 +84,12 @@ elif [[ "$luks" == "y" ]]; then
   luks="1"
   echo -n "LUKS password: "
   read lukspasswd
+  echo ""
   
   # Ask for TPM unlocking (again).
   echo -n "Do you want tpm unlocking (Requires TPM 2.0)? (y/N) "
   read -n 1 tpm
+  echo ""
   if [[ "$tpm" == "Y" ]]; then
     tpm="1"
   elif [[ "$tpm" == "y" ]]; then
@@ -95,8 +113,10 @@ fi
 # Ask for account passwords.
 echo -n "Password for sysop account (Admin): "
 read sysop
-echo -n "Password for non-sudo account (non-admin): "
-read non-sudo
+echo ""
+echo -n "Password for non_sudo account (non-admin): "
+read non_sudo
+echo ""
 
 # Start partitioning
 parted $drive --script 'mklabel gpt mkpart "EFI system partition" fat32 1MiB 1025MiB set 1 esp on mkpart "root" fat32 1025MiB 100% type 2 4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709'
@@ -126,6 +146,7 @@ pacman -Sy
 # Start off the beggining of the file for the seccond stage within chroot.
 echo "#!/usr/bin/env bash" > /mnt/stage2.sh
 echo "drive=${drive}" >> /mnt/stage2.sh
+echo "firewall=${firewall}" >> /mnt/stage2.sh
 echo "partition1=${partition1}" >> /mnt/stage2.sh
 echo "partition2=${partition2}" >> /mnt/stage2.sh
 echo "luks=${luks}" >> /mnt/stage2.sh
@@ -135,15 +156,43 @@ echo "tpm=${tpm}" >> /mnt/stage2.sh
 echo "vbox=${vbox}" >> /mnt/stage2.sh
 echo "sysop=${sysop}" >> /mnt/stage2.sh
 echo "non-sudo=${non-sudo}" >> /mnt/stage2.sh
-echo "$firewall=${firewall}" >> /mnt/stage2.sh
+echo "firewall=${firewall}" >> /mnt/stage2.sh
+echo "hostname=${hostname}" >> /mnt/stage2.sh
+echo "locale_string=${locale_string}" >> /mnt/stage2.sh
+echo "ln -sf /usr/share/zoneinfo/UTC /etc/localtime" >> /mnt/stage2.sh
+echo "sed -i 's/#\${locale_string}'/\${locale_string" >> /mnt/stage2.sh
 chmod +x /mnt/stage2.sh
 
-# Begin the install.
-if [ "$vbox" == "1" ] && [ "$firewall" == "1" ] && [ "$luks" == "1" ] && [ "$tpm" == "1" ]; then
-
-elif [ ""
-
+# Begin setup of the install list.
+echo -n "" > ./install_list.txt
+echo "base" >> ./install_list.txt
+echo "linux-hardened" >> ./install_list.txt
+echo "linux-firmware" >> ./install_list.txt
+echo "gvim" >> ./install_list.txt
+echo "grub" >> ./install_list.txt
+echo "efibootmgr" >> ./install_list.txt
+echo "lightdm" >> ./install_list.txt
+echo "networkmanager" >> ./install_list.txt
+echo "seahorse" >> ./install_list.txt
+echo "gnome-keyring" >> ./install_list.txt
+echo "nm-connection-editor" >> ./install_list.txt
+echo "sublime-text" >> ./install_list.txt
+echo "cinnamon" >> ./install_list.txt
+if [ "$vbox" == "1" ]; then
+  echo "virtual-box-guest-utils" >> ./install_list.txt
 fi
+if [ "$tpm" == "1" ]; then
+  echo "clevis" >> ./install_list.txt
+fi
+if [ "$firewall" == "1" ]; then
+  echo "nftables" >> ./install_list.txt
+fi
+echo -n "sudo" >> ./install_list.txt
+cat ./install_list.txt | tr '\n' ' ' > ./install_list.new.txt && rm ./install_list.txt && mv ./install_list.new.txt ./install_list.txt
+
+# Begin install
+pacstrap -K /mnt $(echo $(cat ./install_list.txt))
+genfstab -U /mnt/etc/fstab
 
 # Tack on during chroot for vm.
 #echo "GSK_RENDERER=gl" >> /etc/environment
