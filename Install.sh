bdffc1ecd5afeead3321db05e2337f347c715ade
216321 <110435555+216321@users.noreply.github.com>
2025-04-07 03:02:30 +0000

    Fixed syntax issues

--- Install.sh
+++ Install.sh
@@ -36,7 +36,7 @@ read -n 1 vbox
 echo ""
 if [[ "$vbox" == "Y" ]]; then
   vbox="1"
-elif [[ "$tpm" == "y" ]]; then
+elif [[ "$vbox" == "y" ]]; then
   vbox="1"
 else
   vbox="0"
@@ -161,22 +161,26 @@ echo "hostname=${hostname}" >> /mnt/stage2.sh
 echo "locale_string=${locale_string}" >> /mnt/stage2.sh
 echo "ln -sf /usr/share/zoneinfo/UTC /etc/localtime" >> /mnt/stage2.sh
 echo "hwclock --systohc" >> /mnt/stage2.sh
-echo "sed -i 's/#\${locale_string}'/\${locale_string}" >> /mnt/stage2.sh
+echo 'sed -i "s/#\${locale_string}/\${locale_string}" "/etc/locale.gen' >> /mnt/stage2.sh
 echo "locale-gen" >> /mnt/stage2.sh
-echo "echo 'LANG=\${locale_string}' >> /etc/locale.conf" >> /mnt/stage2.sh
-echo "echo 'reverse_system' >> /etc/hostname" >> /mnt/stage2.sh
+echo 'echo "LANG=\${locale_string}" >> /etc/locale.conf' >> /mnt/stage2.sh
+echo 'echo "\${hostname}" >> /etc/hostname' >> /mnt/stage2.sh
 echo "useradd -m sysop" >> /mnt/stage2.sh
 echo "useradd -m non_sudo" >> /mnt/stage2.sh
 echo "usermod -aG wheel sysop" >> /mnt/stage2.sh
-echo "echo '\${sysop}' | passwd --stdin sysop" >> /mnt/stage2.sh
-echo "echo '\${non_sudo}' | passwd --stdin non_sudo" >> /mnt/stage2.sh
+echo 'echo "\${sysop}" | passwd --stdin sysop' >> /mnt/stage2.sh
+echo 'echo "\${non_sudo}" | passwd --stdin non_sudo' >> /mnt/stage2.sh
 echo "systemctl enable NetworkManager.service" >> /mnt/stage2.sh
+# Sudo stuff
+echo "echo '%wheel  ALL=(ALL:ALL) ALL' >> /etc/sudoers"
+# VM check based on previous input
 echo "if [[ \$vbox == '1' ]]; then" >> /mnt/stage2.sh
 echo "  systemctl enable vboxservice.service" >> /mnt/stage2.sh
 echo "fi" >> /mnt/stage2.sh
+# Grub install
 echo "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB" >> /mnt/stage2.sh
 echo "mkinitcpio -P" >> /mnt/stage2.sh
-echo "grub-mkconfig -o /boot/grub.cfg" >> /mnt/stage2.sh
+echo "grub-mkconfig -o /boot/grub/grub.cfg" >> /mnt/stage2.sh
 chmod +x /mnt/stage2.sh
 
 # Begin setup of the install list.
